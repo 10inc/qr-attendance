bf27d80fe81126616f2ce667f3d0f488b383a371
Ethan Sonza <ethanjericho@yahoo.com>
2021-11-29 22:28:23 +1000

    fix icons

--- mobile/src/views/Dashboard.js
+++ mobile/src/views/Dashboard.js
@@ -1,12 +1,30 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
+import {Appbar} from 'react-native-paper';
 
-const Dashboard = ({history}) => {
+import {useAuth} from '../auth';
+
+const Dashboard = () => {
+  const {signOut} = useAuth();
+
   return (
     <View>
+      <Appbar style={styles.top}>
+        <Appbar.Action icon="logout" onPress={signOut} />
+      </Appbar>
+
       <Text>HELLO WORLD</Text>
     </View>
   );
 };
 
 export default Dashboard;
+
+const styles = StyleSheet.create({
+  top: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+  },
+});
