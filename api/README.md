97c91028c64c1473fc04785eb313e924f322e416
Ethan Sonza <ethanjericho@yahoo.com>
2021-12-06 01:24:00 +1000

    prod use google smtp

--- api/README.md
+++ api/README.md
@@ -5,7 +5,11 @@
 # Setup
 - Configure jwt tokens and smtp settings at `./config.json`
 - Secret is used for JWT tokens. Can be any string.
-- Create a fake smtp service with https://mailtrap.io/ or https://ethereal.email
+- For emails either:
+  - Create a fake smtp service with https://mailtrap.io/ or https://ethereal.email
+    - Set env vars: `MAILTRAP_USER`, `MAILTRAP_PASS`
+  - or use a Google account with less secure apps enables
+    - Set env vars: `GOOGLE_SMTP_USER`, `GOOGLE_SMTP_PASS`
 
 # Run the app
 - npm ci
--- api/_helpers/send-email.js
+++ api/_helpers/send-email.js
@@ -3,7 +3,24 @@ const config = require('config.json');
 
 module.exports = sendEmail;
 
+// const smtpMailtrap = {
+//     host: "smtp.mailtrap.io",
+//     port: 2525,
+//     auth: {
+//         user: process.env.MAILTRAP_USER,
+//         pass: process.env.MAILTRAP_PASS
+//     }
+// }
+
+const smtpGoogle = {
+    service: 'gmail',
+    auth: {
+        user: process.env.GOOGLE_SMTP_USER,
+        pass: process.env.GOOGLE_SMTP_PASS,
+    }
+}
+
 async function sendEmail({ to, subject, html, from = config.emailFrom, attachments=[] }) {
-    const transporter = nodemailer.createTransport(config.smtpOptions);
+    const transporter = nodemailer.createTransport(smtpGoogle);
     await transporter.sendMail({ from, to, subject, html, attachments });
 }
