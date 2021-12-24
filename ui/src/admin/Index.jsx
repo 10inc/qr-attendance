087958c8d543bfa8394a137b769d9412115a179f
Ethan Sonza <ethanjericho@yahoo.com>
2021-12-24 16:07:14 +1000

    admin page but put off

--- ui/src/admin/Index.jsx
+++ ui/src/admin/Index.jsx
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Route, Switch } from 'react-router-dom';
-
+import { AppBar, Box, Button, Toolbar } from '@mui/material';
+import { useHistory } from 'react-router-dom';
 import { Overview } from './Overview';
 import { Users } from './users';
 import { Students } from './students';
 import { Events } from './events';
 
 function Admin({ match }) {
-    const { path } = match;
+  const { path } = match;
+  const history = useHistory()
 
-    return (
-        <div className="p-4">
-            <div className="container">
-                <Switch>
-                    <Route exact path={path} component={Overview} />
-                    <Route path={`${path}/users`} component={Users} />
-                    <Route path={`${path}/students`} component={Students} />
-                    <Route path={`${path}/events`} component={Events} />
-                </Switch>
-            </div>
-        </div>
-    );
+  return (
+    <Box>
+      <AppBar position="static" sx={{ px: 1 }}>
+          <Toolbar disableGutters>
+          {['users', 'students', 'events'].map((item) =>
+            <Button
+              onClick={() => history.push(`${path}/${item}`)}
+              sx={{ color: 'white' }}
+            >
+              {item.charAt(0).toUpperCase() + item.slice(1)}
+            </Button>
+          )}
+          </Toolbar>
+      </AppBar>
+      <Switch>
+        <Route exact path={path} component={Overview} />
+        <Route path={`${path}/users`} component={Users} />
+        <Route path={`${path}/students`} component={Students} />
+        <Route path={`${path}/events`} component={Events} />
+      </Switch>
+      {/* 
+        TODO:
+        - generic profile (optional user prop, else current user)
+        - fe for overview (my profile), users, students, events
+      */}
+    </Box>
+  );
 }
 
 export { Admin };
