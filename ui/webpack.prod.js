20d90288ab72c8485700ed8de5d49eda6ddbb69b
Ethan Sonza <ethanjericho@yahoo.com>
2021-12-27 23:04:18 +1000

    resolve build

--- ui/webpack.prod.js
+++ ui/webpack.prod.js
@@ -1,14 +1,12 @@
 const path = require('path')
 const { merge } = require('webpack-merge')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
-const TerserPlugin = require('terser-webpack-plugin')
 const commonConfig = require('./webpack.common.js')
 
 module.exports = merge(commonConfig, {
   mode: 'production',
   output: {
-    filename: '[name].[chunkhash].js',
+    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist')
   },
   devtool: 'source-map',
@@ -28,24 +26,5 @@ module.exports = merge(commonConfig, {
       filename: '[name].[contenthash].css',
     }),
   ],
-  optimization: {
-    minimizer: [
-      new OptimizeCssAssetsPlugin({
-        cssProcessorOptions: {
-          map: {
-            inline: false,
-            annotation: true,
-          },
-        },
-      }),
-      new TerserPlugin({
-        // Use multi-process parallel running to improve the build speed
-        // Default number of concurrent runs: os.cpus().length - 1
-        parallel: true,
-        // Enable file caching
-        cache: true,
-        sourceMap: true,
-      }),
-    ],
-  },
+  optimization: {},
 })
